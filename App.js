2dc418a9a67f7b86a9f7d410d9b8774e8df88f89
Chandra Gurnadham <gurnadham19@gmail.com>
2024-10-16 22:44:56 +0530

    reactintialsetup

---
+++ App.js
@@ -0,0 +1,32 @@
+
+{/* <div id="parent">
+    <div id="child">
+        <h1>im h1 tag</h1>
+        <h2>im h2 tag</h2>
+        </div>
+         <div id="child2">
+        <h1>im h1 tag</h1>
+        <h2>im h2 tag</h2>
+        </div>
+        </div> */}
+
+
+const parent=React.createElement("div",
+    {id:"parent"},
+   [ React.createElement("div",{id:"child"},
+        [React.createElement("h1",{},"im h1 tag"),React.createElement("h2",{},"im h2 tag")]
+),React.createElement("div",{id:"child2"},
+    [React.createElement("h1",{},"im h1 tag"),React.createElement("h2",{},"im h2 tag")]
+)]);
+
+
+
+
+const heading = React.createElement(
+    "h1", 
+    {id:"heading"}, 
+    "Hello world, React!!");
+    console.log(parent)   /// it will create object
+
+const root = ReactDOM.createRoot(document.getElementById("root")); 
+root.render(parent);
